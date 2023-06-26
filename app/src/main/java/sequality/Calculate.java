2f6717949fb8c0880277061011a719788dd82a73
bili <biglight258@gmail.com>
2023-06-26 13:30:27 +0900

    add calculate

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -1,7 +1,19 @@
 package sequality;
 
 public class Calculate {
-  public int sum(int x, int y) {
-    return x + y;
+  public String startCalculation() {
+    return "Start Calculation";
+  }
+
+  public int mul(int a, int b) {
+    return a * b;
+  }
+
+  public String oddeven(int a) {
+    if (a % 2 == 0) {
+      return "even";
+    } else {
+      return "odd";
+    }
   }
 }
--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -5,9 +5,21 @@ import static org.junit.Assert.*;
 
 public class CalculateTest {
   @Test
-  public void testSum() {
+  public void teststartCalculation() {
     Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+    assertNotNull("Start Calculation", calculate.startCalculation());
+  }
+
+  @Test
+  public void testmul() {
+    Calculate calculate = new Calculate();
+    int expected = 6;
+    assertEquals(expected, calculate.mul(2, 3));
+  }
+
+  @Test
+  public void testoddeven() {
+    Calculate calculate = new Calculate();
+    assertNotNull("odd", calculate.oddeven(6));
   }
 }
